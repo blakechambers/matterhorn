acfda0267dde605d7ed8a6ca47c452c5f45f35b7
Blake Chambers <chambb1@gmail.com>
2015-05-14 11:43:48 -0400

    Removing pendings

--- spec/lib/matterhorn/links/link_set_spec.rb
+++ spec/lib/matterhorn/links/link_set_spec.rb
@@ -8,14 +8,4 @@ RSpec.describe "Matterhorn::Links::LinkSet" do
     expect(link_set).to respond_to(:as_json)
   end
 
-  context "#serializable_hash" do
-
-    context "with self link in the set" do
-
-      it "should have a self link when serialized"
-
-    end
-
-  end
-
 end
